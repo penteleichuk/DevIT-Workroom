e3108d73853335706b4edb5a0aad64bc5196f49b
vasyok28 <waitoatime@gmail.com>
2022-11-21 17:25:18 +0200

    add demensions

--- src/components/containers/ScreenContainer/ScreenContainerStyle.ts
+++ src/components/containers/ScreenContainer/ScreenContainerStyle.ts
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
+import { CONTENT_HORIZONTAL_PADDING } from "../../../helpers/demensions";
 
 export const styles = StyleSheet.create({
-  safeArea: {flex: 1},
-  container: {},
+  safeArea: { flex: 1 },
+  container: {
+    paddingHorizontal: CONTENT_HORIZONTAL_PADDING,
+  },
 });
---
+++ src/helpers/demensions.ts
@@ -0,0 +1,23 @@
+import { Dimensions } from "react-native";
+
+import {
+  scale as SCALE,
+  verticalScale as VERTICAL_SCALE,
+  moderateScale as MODERATE_SCALE,
+} from "react-native-size-matters";
+
+// Will return a linear scaled result of the provided size, based on your device's screen width. (width)
+export const scale = SCALE;
+
+// Will return a linear scaled result of the provided size, based on your device's screen height. (height)
+export const verticalScale = VERTICAL_SCALE;
+
+// Sometimes you don't want to scale everything in a linear manner, that's where moderateScale comes in.
+// The cool thing about it is that you can control the resize factor (default is 0.5).
+// If normal scale will increase your size by +2X, moderateScale will only increase it by +X, for example: (padding)
+export const moderateScale = MODERATE_SCALE;
+
+export const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
+
+// SIZE
+export const CONTENT_HORIZONTAL_PADDING = scale(32);
