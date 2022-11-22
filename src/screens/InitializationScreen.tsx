6ca11143fa7b17d237d96e14012fcff88cc3bbc5
vasyok28 <waitoatime@gmail.com>
2022-11-22 16:21:48 +0200

    clear init component

--- src/screens/InitializationScreen.tsx
+++ src/screens/InitializationScreen.tsx
@@ -1,32 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { View } from "react-native";
-import {
-  Button,
-  CodeInput,
-  PhoneInput,
-  ScreenContainer,
-  TextInput,
-  TextInputSecret,
-} from "../components";
+import { ScreenContainer } from "../components";
 
 export const InitializationScreen = React.memo(() => {
-  const [value, setValue] = useState("");
-  const [number, setNumber] = useState("");
-  const [validValue, setValidValue] = useState(false);
-
   return (
     <ScreenContainer>
-      <View>
-        <TextInput placeholder="Your Email" />
-        <TextInputSecret placeholder="Password" />
-        <CodeInput value={value} setValue={setValue} />
-        <PhoneInput
-          value={number}
-          setValue={setNumber}
-          setValidValue={setValidValue}
-        />
-        <Button title="Log In" />
-      </View>
+      <View></View>
     </ScreenContainer>
   );
 });
