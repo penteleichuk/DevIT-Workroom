1201e0bd8a47706e7ed349ecfaebf4a366c102ce
vasyok28 <waitoatime@gmail.com>
2022-11-23 16:10:39 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Navigation } from "./src/routes";
 
+// Хардкор без глобального стейту 😀
+// Був би не проти фідбека :)
+// Багато тестових, тести не встиг😇
+
 export default function App() {
   return <Navigation />;
 }
